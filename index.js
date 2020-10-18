0cec89dbcc55684eff6a36383765b6b4a64464c0
Marco8301 <marcanmalas@gmail.com>
2020-10-18 16:44:14 +0200

    twelve

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use(middlewares.notFoundMiddleware);
 app.use(middlewares.errorsMiddleware);
 
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
